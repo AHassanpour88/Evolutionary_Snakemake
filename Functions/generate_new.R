3ab829edcfcb706d835eb6c4b1de1e53e36d4c65
A.Hassanpour <77358339+AHassanpour88@users.noreply.github.com>
2024-01-14 14:43:11 +0100

    Update generate_new.R

--- Functions/generate_new.R
+++ Functions/generate_new.R
@@ -1,55 +1,14 @@
-# generate_new <- function(){
-#   x <- numeric(12)
-#   x[1] <- rbinom(1,1,0.5)
-#   x[2] <- rbinom(1,1,0.5)
-#   x[3] <- sample(25000:40000,1)
-#   x[4] <- sample(25000:40000,1)
-#   x[5] <- sample(1000:3000, 1)
-#   x[6] <- sample(1000:3000, 1)
-#   x[7] <- sample(150:400, 1)
-#   x[8] <- sample(150:400, 1)
-#   x[9] <- sample(15:50, 1)
-#   x[10] <- sample(15:50, 1)
-#   x[11] <- sample(250:500, 1)
-#   x[12] <- sample(250:500, 1)
-#   
-#   new_cost <- calc_cost(x)
-#   
-#   
-#   # If costs are to high all parameters will be scaled down
-#   #5000 as long as the cost is very different between this program and are base-line cost we need to make changes
-#   #if we are really close we can stop
-#   #this leads to not having the same exact cost,
-#   index <- 1
-#   while(abs(new_cost-base_cost)>5000 & index < 20){
-#     x[cost_par] <- round(x[cost_par] * base_cost / new_cost)
-#     new_cost <- calc_cost(x)
-#     index <- index + 1
-#   }
-#   
-#   
-#   # For the fine tuning number will be adopted as they are least costly
-#   # Cheapest unit in breeding is DH ones to compensate the rest for scaling
-#   if(new_cost!=base_cost){
-#     if(rbinom(1,1,0.5)==1){
-#       x[cheapest_unit_male] <- x[cheapest_unit_male] - round((new_cost - base_cost) / cost_cheapest_male) -1
-#     } else{
-#       x[cheapest_unit_female] <- x[cheapest_unit_female] - round((new_cost - base_cost) / cost_cheapest_female) -1
-#     }
-#   }
-#   
-#   return(x)
-# }
-
-
 generate_new <- function(){
+  ### this can be as exact as the initial bound in sampleScript or new bounds
+  ### As initial bounds exclude the area of optimum we add them here to check a broader range
+  ### this will be activated only if n_off_random in iteration.csv in config folder will be > 0 
+  x <- numeric(4)
+  x[4] <- rbinom(1,1,0.5)
+  x[1] <- sample(100:700,1)
+  x[2] <- floor((10000000-3000*x[1])/(4000 + 1000*x[4]))
+  x[3] <- sample(15:25, 1)
 
-x <- numeric(3)
-x[1] <- sample(300:500,1)
-x[2] <- floor((10000000-3000*x[1])/4000)
-x[3] <- sample(10:20, 1)
-
-x <- c(x[2],x[1],x[3])
-
+  x <- c(x[2],x[1],x[3],x[4])
+  
   return(x)
 }
