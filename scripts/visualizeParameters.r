92d7d260f67536e4a95d63a8eb2df4250e0e2069
A.Hassanpour <77358339+AHassanpour88@users.noreply.github.com>
2024-01-14 14:12:20 +0100

    Update visualizeParameters.r
    parameter_convergence along with the target function improvement

--- scripts/visualizeParameters.r
+++ scripts/visualizeParameters.r
@@ -3,44 +3,78 @@ output <- args[1]
 files <- args [-1]
 
 library(yaml)
+library(ggplot2)
 config <- yaml.load_file("config/config.yaml")
 
 nfactors = config$nfactors
 my_target = config$my_target
 target = unlist(config$target)
+names <- config$name_parameter
 
 load("EvoStatus/EvoStatus.RData")
-# Calculate the number of rows and columns needed for the plot
-nrows <- ceiling(sqrt(nfactors))
-ncols <- ceiling(nfactors/nrows)
+iteration <- evolutionary_log$n.iteration
 
-# Set the mfrow parameter based on the calculated values
+#################################################################################
+############################# Parameters convergence ############################
+pdf(output)
 par(mfrow=c(nrows, ncols))
-pdf("Convergence.pdf")
-
 
 if(my_target){
   for(index in 1:nfactors){
-    plot(evolutionary_log$center[index,], type="l", ylim = c(min(target[index], min(evolutionary_log$center[index,]), na.rm = TRUE),
-                                                             max(target[index], max(evolutionary_log$center[index,]), na.rm=TRUE)))
-    
-    # lines(evolutionary_log$est_pos[index,] , ylim=c(99000, 100500), col="green")
-    if(!is.na(target[index])){abline(h=target[index], col="red", lwd=2)}
-    
-  }
-  
-}else{
-  for(index in 1:3){
-    plot(evolutionary_log$center[index,], type="l"
-    #      , ylim = c(min(target[index], min(evolutionary_log$center[index,]), na.rm = TRUE),
-    #                 max(target[index], max(evolutionary_log$center[index,]), na.rm=TRUE))
-         )
-    
-    # lines(evolutionary_log$est_pos[index,] , ylim=c(99000, 100500), col="green")
-    # if(!is.na(target[index])){abline(h=target[index], col="red", lwd=2)}
-    
+    if (my_target) {
+      ggplot() +
+        geom_line(aes(x = 1:iteration, y = evolutionary_log$convergence[index, 1:iteration]), size = 1, linetype = "solid") +
+        ggtitle(names[index]) +
+        theme_minimal() + 
+        xlab("Iteration") + ylab("Optimal Value") +
+        theme(
+          plot.title = element_text(hjust = 0.5),
+          axis.text = element_text(size = 13),
+          axis.title = element_text(size = 16),
+          legend.text = element_text(size = 14),
+          legend.title = element_text(size = 13, face = "bold"),
+          panel.border = element_rect(color = "black", fill = NA, size = 1.2)
+        ) +
+        scale_x_continuous(breaks = seq(1, iteration)) +
+        geom_hline(yintercept = target[index], color = "black", linetype = "solid", size = 1) +
+        geom_point(aes(x = 1:iteration, y = evolutionary_log$convergence[index, 1:iteration]), color = "red", size = 3) +
+        geom_path(aes(x = 1:iteration, y = evolutionary_log$convergence[index, 1:iteration]), color = "red", size = 1)
+    } else {
+      ggplot() +
+        geom_line(aes(x = 1:iteration, y = evolutionary_log$convergence[index, 1:iteration]), size = 1, linetype = "solid") + 
+        ggtitle(names[index]) +
+        theme_minimal() + 
+        xlab("Iteration") + ylab("Optimal Value") +
+        theme(
+          plot.title = element_text(hjust = 0.5),
+          axis.text = element_text(size = 13),
+          axis.title = element_text(size = 16),
+          legend.text = element_text(size = 14),
+          legend.title = element_text(size = 13, face = "bold"),
+          panel.border = element_rect(color = "black", fill = NA, size = 1.2)
+        ) +
+        scale_x_continuous(breaks = seq(1, iteration)) +
+        geom_point(aes(x = 1:iteration, y = evolutionary_log$convergence[index, 1:iteration]), color = "red", size = 3) +
+        geom_path(aes(x = 1:iteration, y = evolutionary_log$convergence[index, 1:iteration]), color = "red", size = 1)
+    }
   }
 }
-
-
+#################################################################################
+# track how much the objectvie function improved from frst iteration
+plot(evolutionary_log$est_approx_avg - evolutionary_log$est_approx_avg[1],
+     type = "o",
+     ylab = "Objective Function",
+     xlab = "Iteration"
+)
+title(main = "Evolution of Target Function Over Iterations using kernel regression")
+#################################################################################
+# track how the variation of objective function over iteration
+# # high variance indicates that individuals are in different part o search space and we are still not in a refinement phase
+plot(evolutionary_log$est_approx_sd,
+     type = "o",
+     ylab = "Variance",
+     xlab = "Iteration"
+)
+title(main = "Variance of Objective Function Over Iterations")
+#################################################################################
 dev.off()
