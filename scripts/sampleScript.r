e424f91a2b8cd03609521ab880433abd2d92d195
A.Hassanpour <77358339+AHassanpour88@users.noreply.github.com>
2024-07-18 10:29:50 +0200

    Update sampleScript.r

--- scripts/sampleScript.r
+++ scripts/sampleScript.r
@@ -1,3 +1,21 @@
+#################################################################################
+################################# Agreement #####################################
+#################################################################################
+# Authors
+# Azadeh Hassanpour, azadeh.hassanpour@uni-goettingen.de
+# Johannes Geibel, johannes.geibel@fli.de
+# Torsten Pook, Torsten.pook@wur.nl
+# Copyright © 2020 – 2024
+# This program falls under a NonCommercial-NoDerivates-NoDistriubtion Public License.
+# With use of these scripts, I confirm that I represent an academic institute and acknowledge that I shall use this script only for research. I explicitly acknowledge the terms in the license agreement https://github.com/AHassanpour88/Evolutionary_Snakemake/blob/main/License.md. I understood that any commercial use needs a commercial license from the owner of the script. For more information about a commercial license please contact Torsten Pook.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# 
+#################################################################################
+#################################################################################
+#################################################################################
+
 args <- commandArgs(TRUE)
 output <- args[1]
 
@@ -14,9 +32,9 @@ results <- matrix(0, nrow=sim, ncol=nfactors)
 #random sampling of initial parameters
 for(index in 1:sim){
   MIR <- rbinom(1,1,0.5)
-  n_bull <- sample(300:500,1)
+  n_bull <- sample(100:700,1)
   n_test <- floor((10000000-3000*n_bull)/(4000 + 1000*MIR))
-  n_bull_sel <- sample(15:25, 1)
+  n_bull_sel <- sample(3:30, 1)
 ## sort the variable below as it appears in config$name_parameter 
   results[index,] <- c(n_test,n_bull,n_bull_sel,MIR)
 }
