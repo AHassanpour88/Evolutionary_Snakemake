3a24a242d136cc281a39382b692bf51a601a6eae
A.Hassanpour <77358339+AHassanpour88@users.noreply.github.com>
2024-07-18 14:08:44 +0200

    Update evoScript.r

--- scripts/evoScript.r
+++ scripts/evoScript.r
@@ -7,7 +7,9 @@
 # Torsten Pook, Torsten.pook@wur.nl
 # Copyright © 2020 – 2024
 # This program falls under a NonCommercial-NoDerivates-NoDistriubtion Public License.
-# With use of these scripts, I confirm that I represent an academic institute and acknowledge that I shall use this script only for research. I explicitly acknowledge the terms in the license agreement https://github.com/AHassanpour88/Evolutionary_Snakemake/blob/main/License.md. I understood that any commercial use needs a commercial license from the owner of the script. For more information about a commercial license please contact Torsten Pook.
+# With use of these scripts, I confirm that I represent an academic institute and acknowledge that I shall use this script only for research. 
+# I explicitly acknowledge the terms in the license agreement https://github.com/AHassanpour88/Evolutionary_Snakemake/blob/main/License.md.
+# I understood that any commercial use needs a commercial license from the owner of the script. For more information about a commercial license please contact Torsten Pook.
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
@@ -47,7 +49,7 @@ sel_tmp_row <- which(evoParameters$iteration_start <= iteration & evoParameters$
 n_sel_high_target <- evoParameters$n_sel_high_target[sel_tmp_row]
 # we recommend to select at least 10% of the parents from high performing candidates using kernel regression method
 n_sel_kernel <- evoParameters$n_sel_kernel[sel_tmp_row]
-# Use some high performing candidates from second last pool to don't loose diversity
+# Use some high performing candidates from second last pool to don't lose diversity
 n_sel_last2 <- evoParameters$n_sel_last2[sel_tmp_row]
 # the number of total parents and also the first batch of offsprings will be exactly these parents
 n_sel <- n_sel_high_target + n_sel_kernel + n_sel_last2
