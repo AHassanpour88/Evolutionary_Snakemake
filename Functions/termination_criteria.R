e42aa3a779b0edd9d2e094543bd9aeedcbc3b8d3
A.Hassanpour <77358339+AHassanpour88@users.noreply.github.com>
2024-07-18 10:34:33 +0200

    Update termination_criteria.R

--- Functions/termination_criteria.R
+++ Functions/termination_criteria.R
@@ -1,5 +1,26 @@
+#################################################################################
+################################# Agreement #####################################
+#################################################################################
+# Authors
+# Azadeh Hassanpour, azadeh.hassanpour@uni-goettingen.de
+# Johannes Geibel, johannes.geibel@fli.de
+# Torsten Pook, Torsten.pook@wur.nl
+# Copyright © 2020 – 2024
+# This program falls under a NonCommercial-NoDerivates-NoDistriubtion Public License.
+# With use of these scripts, I confirm that I represent an academic institute and acknowledge that I shall use this script only for research. I explicitly acknowledge the terms in the license agreement https://github.com/AHassanpour88/Evolutionary_Snakemake/blob/main/License.md. I understood that any commercial use needs a commercial license from the owner of the script. For more information about a commercial license please contact Torsten Pook.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# 
+#################################################################################
+#################################################################################
+#################################################################################
+
+
+
 ## this function can be changed to any termination criteria the user wants to implement
-# the general termination criteria is when the number of final iteration is reached by the algorthm 
+# the general termination criteria is when the number of final iteration is reached by the algorithm
+# For the example provided we did not set any criteria and we visually observed if the target function is changing or not (this is more efficient because often you do not know what level of threshold is satisfactory!)
 
 termination_criteria <- function(evolutionary_log, 
                                 iteration, 
