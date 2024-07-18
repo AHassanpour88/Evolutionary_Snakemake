028ff1f92ddd768a69017b52960a246cd4a480b9
A.Hassanpour <77358339+AHassanpour88@users.noreply.github.com>
2024-07-18 10:31:42 +0200

    Update visualizeParameters.r

--- scripts/visualizeParameters.r
+++ scripts/visualizeParameters.r
@@ -1,3 +1,22 @@
+#################################################################################
+################################# Agreement #####################################
+#################################################################################
+# Authors
+# Azadeh Hassanpour, azadeh.hassanpour@uni-goettingen.de
+# Johannes Geibel, johannes.geibel@fli.de
+# Torsten Pook, Torsten.pook@wur.nl
+# Copyright © 2020 – 2024
+# This program falls under a NonCommercial-NoDerivates-NoDistriubtion Public License.
+# With use of these scripts, I confirm that I represent an academic institute and acknowledge that I shall use this script only for research. I explicitly acknowledge the terms in the license agreement https://github.com/AHassanpour88/Evolutionary_Snakemake/blob/main/License.md. I understood that any commercial use needs a commercial license from the owner of the script. For more information about a commercial license please contact Torsten Pook.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# 
+#################################################################################
+#################################################################################
+#################################################################################
+
+
 args <- commandArgs(TRUE)
 output <- args[1]
 files <- args [-1]
